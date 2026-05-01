54237892d8017c580fd48bb4a9ed447a489440a6
Omar <omarabumuhfouz@gmail.com>
2026-05-01 15:41:10 +0300

    Add User Aggregate Root and Order Domain Events

--- src/MazadZone.Domain/Orders/Events/FeedbackRepliedDomainEvent.cs
+++ src/MazadZone.Domain/Orders/Events/FeedbackRepliedDomainEvent.cs
@@ -1,9 +1,7 @@
-
 namespace MazadZone.Domain.Orders.Events;
 
 public sealed record FeedbackRepliedDomainEvent(OrderId OrderId, FeedbackId FeedbackId) : IDomainEvent
 {
     public Guid Id => Guid.NewGuid();
-
     public DateTime OccurredOnUtc => DateTime.UtcNow;
 }
