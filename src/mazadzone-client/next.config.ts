15f0e9acf45be368ef10f33903dbdcfc3843a20a
khaled0x1 <120220612151@st.ahu.edu.jo>
2026-06-14 07:49:57 +0300

    feat: update development server host and add sharp dependency
    - Changed the development server command to bind to 0.0.0.0 for accessibility.
    - Added sharp library for image processing capabilities.
    - Introduced a new CSS variable for extra small breakpoints in globals.css.

--- src/mazadzone-client/next.config.ts
+++ src/mazadzone-client/next.config.ts
@@ -10,16 +10,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: "http",
+        hostname: "**",
       },
       {
         protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
+        hostname: "**",
       },
     ],
   },
